a2d8d417a8b0fda0ebd7a81efbf9b72b33e4bed0
shyam gupta <60971003+shyamgupta196@users.noreply.github.com>
2023-07-01 20:23:15 +0000

    model hosted :p , couldnot serve it :(

---
+++ bash.sh
@@ -0,0 +1,3 @@
+curl -X 'GET' \
+  'https://shyamgupta196-zany-xylophone-q455g5gpvg7cx5p4-3000.preview.app.github.dev/healthz' \
+  -H 'accept: */*'
---
+++ infer.py
@@ -0,0 +1,16 @@
+# import bentoml
+
+# loaded_model = bentoml.sklearn.load_model("iris_clf:kzybxoayjgbsuaan")
+
+# result = loaded_model.predict([[5.9, 3. , 5.1, 1.8]])
+# print(result)
+
+import requests
+
+result = requests.post(
+   "http://127.0.0.1:3000/classify",
+   headers={"content-type": "application/json"},
+   data="[[5.9, 3, 5.1, 1.8]]",
+).text
+
+print(result)
